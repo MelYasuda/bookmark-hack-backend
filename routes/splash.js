a61c79692bcdcde0908960d85e2832b8f73c5945
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2019-05-21 13:31:30 -0700

    before 2nd deploy

--- routes/splash.js
+++ routes/splash.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send({obj: 'Splash page'});
+  res.send({obj: 'Splash'});
 });
 
 module.exports = router;
